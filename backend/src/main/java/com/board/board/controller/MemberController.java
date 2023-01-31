a4cc3f90a4c7f9dab08f98a4e4f4d41fb210c5f4
zzzdks760 <zzzdks760@naver.com>
2023-01-31 20:46:42 +0900

    .

--- backend/src/main/java/com/board/board/controller/MemberController.java
+++ backend/src/main/java/com/board/board/controller/MemberController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-
+@CrossOrigin("http://localhost:8585, http://localhost:8081")
 public class MemberController {
 
     private final MemberService memberService;
@@ -21,58 +21,60 @@ public class MemberController {
     //회원가입
     @PostMapping("/member/save") // 값을 받아올 페이지
     public String save(@RequestBody @Valid MemberDTO memberDTO) {
-       return "ok";
+        System.out.println("MemberController.save");
+        System.out.println("memberDTO = " + memberDTO);
+        return memberService.save(memberDTO);
     }
     // 로그인
-    @PostMapping("/member/login")
-    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
-        MemberDTO loginResult = memberService.login(memberDTO);
-        if (loginResult != null) {
-            // login 성공
-            session.setAttribute("loginEmail", loginResult.getMemberEmail());
-            return "?";
-        } else {
-          // login 실패
-          return "?";
-        }
-    }
-
-    // 회원 목록 (list라는 페이지에 값을 넘기는 방식 사용)
-    @GetMapping("/member/")
-    public String findAll(Model model) {
-        List<MemberDTO> memberDTOList = memberService.findAll();
-        model.addAttribute("memberList", memberDTOList);
-        return "list";
-
-    }
-
-    // 회원상세 조회 detail Html 리턴
-    @GetMapping("/member/{id}")
-    public String findById(@PathVariable Long id, Model model) {
-        MemberDTO memberDTO = memberService.findById(id);
-        model.addAttribute("member", memberDTO);
-        return  "detail";
-    }
-
-    // 회원정보 수정, update Html 리턴
-    @GetMapping("/member/update")
-    public String updateForm(HttpSession session, Model model) {
-        String myEmail = (String) session.getAttribute("loginEmail");
-        MemberDTO memberDTO = memberService.updateForm(myEmail);
-        model.addAttribute("updateMember", memberDTO);
-        return "update";
-    }
-
-    @PostMapping("/member/update")
-    public String update(@ModelAttribute MemberDTO memberDTO) {
-        memberService.update(memberDTO);
-        return "redirect:/member/" + memberDTO.getId();
-    }
-
-    @GetMapping("/member/delete/{id}")
-    public String deleteById(@PathVariable Long id) {
-        memberService.deleteById(id);
-        return "redirect:/member/";
-    }
-
+//    @PostMapping("/member/login")
+//    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+//        MemberDTO loginResult = memberService.login(memberDTO);
+//        if (loginResult != null) {
+//            // login 성공
+//            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+//            return "?";
+//        } else {
+//          // login 실패
+//          return "?";
+//        }
+//    }
+//
+//    // 회원 목록 (list라는 페이지에 값을 넘기는 방식 사용)
+//    @GetMapping("/member/")
+//    public String findAll(Model model) {
+//        List<MemberDTO> memberDTOList = memberService.findAll();
+//        model.addAttribute("memberList", memberDTOList);
+//        return "list";
+//
+//    }
+//
+//    // 회원상세 조회 detail Html 리턴
+//    @GetMapping("/member/{id}")
+//    public String findById(@PathVariable Long id, Model model) {
+//        MemberDTO memberDTO = memberService.findById(id);
+//        model.addAttribute("member", memberDTO);
+//        return  "detail";
+//    }
+//
+//    // 회원정보 수정, update Html 리턴
+//    @GetMapping("/member/update")
+//    public String updateForm(HttpSession session, Model model) {
+//        String myEmail = (String) session.getAttribute("loginEmail");
+//        MemberDTO memberDTO = memberService.updateForm(myEmail);
+//        model.addAttribute("updateMember", memberDTO);
+//        return "update";
+//    }
+//
+//    @PostMapping("/member/update")
+//    public String update(@ModelAttribute MemberDTO memberDTO) {
+//        memberService.update(memberDTO);
+//        return "redirect:/member/" + memberDTO.getId();
+//    }
+//
+//    @GetMapping("/member/delete/{id}")
+//    public String deleteById(@PathVariable Long id) {
+//        memberService.deleteById(id);
+//        return "redirect:/member/";
+//    }
+//
 }
--- backend/src/main/java/com/board/board/service/MemberService.java
+++ backend/src/main/java/com/board/board/service/MemberService.java
@@ -21,66 +21,66 @@ public class MemberService {
         // 2. repository의 save 메서드 호출
         MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
         memberRepository.save(memberEntity);
-        return "ok";
+        return save(memberDTO);
     }
 
 
-    public MemberDTO login(MemberDTO memberDTO) {
-        /*
-            1. 회원이 입력한 이메일로 DB에서 조회를 함
-            2. DB에서 조회한 비밀번호와 사용자가 입력한 비밀번호가 일치하는지 판단
-        */
-        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
-        if (byMemberEmail.isPresent()) {
-            // 이메일 조회 결과가 있다면
-            MemberEntity memberEntity = byMemberEmail.get();
-            if (memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())) {
-                // 비밀번호 일치
-                // entity -> dto 변환 후 리턴
-                MemberDTO dto = MemberDTO.toMemberDTO(memberEntity);
-                return dto;
-            } else {
-                // 비밀번호 불일치
-                return null;
-            }
-        } else {
-            // 이메일 조회 결과가 없다면
-            return null;
-        }
-    }
-
-    public List<MemberDTO> findAll() {
-        List<MemberEntity> memberEntityList = memberRepository.findAll();
-        List<MemberDTO> memberDTOList = new ArrayList<>();
-        for (MemberEntity memberEntity: memberEntityList) {
-            memberDTOList.add(MemberDTO.toMemberDTO(memberEntity));
-        }
-        return memberDTOList;
-    }
-
-    public MemberDTO findById(Long id) {
-        Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(id);
-        if (optionalMemberEntity.isPresent()) {
-            return MemberDTO.toMemberDTO(optionalMemberEntity.get());
-        }else {
-            return null;
-        }
-    }
-
-    public MemberDTO updateForm(String myEmail) {
-        Optional<MemberEntity> optionalMemberEntity = memberRepository.findByMemberEmail(myEmail);
-        if(optionalMemberEntity.isPresent()) {
-            return MemberDTO.toMemberDTO(optionalMemberEntity.get());
-        } else {
-            return null;
-        }
-    }
-
-    public void update(MemberDTO memberDTO) {
-        memberRepository.save(MemberEntity.toMemberEntity(memberDTO));
-    }
-
-    public void deleteById(Long id) {
-        memberRepository.deleteById(id);
-    }
+//    public MemberDTO login(MemberDTO memberDTO) {
+//        /*
+//            1. 회원이 입력한 이메일로 DB에서 조회를 함
+//            2. DB에서 조회한 비밀번호와 사용자가 입력한 비밀번호가 일치하는지 판단
+//        */
+//        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+//        if (byMemberEmail.isPresent()) {
+//            // 이메일 조회 결과가 있다면
+//            MemberEntity memberEntity = byMemberEmail.get();
+//            if (memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())) {
+//                // 비밀번호 일치
+//                // entity -> dto 변환 후 리턴
+//                MemberDTO dto = MemberDTO.toMemberDTO(memberEntity);
+//                return dto;
+//            } else {
+//                // 비밀번호 불일치
+//                return null;
+//            }
+//        } else {
+//            // 이메일 조회 결과가 없다면
+//            return null;
+//        }
+//    }
+//
+//    public List<MemberDTO> findAll() {
+//        List<MemberEntity> memberEntityList = memberRepository.findAll();
+//        List<MemberDTO> memberDTOList = new ArrayList<>();
+//        for (MemberEntity memberEntity: memberEntityList) {
+//            memberDTOList.add(MemberDTO.toMemberDTO(memberEntity));
+//        }
+//        return memberDTOList;
+//    }
+//
+//    public MemberDTO findById(Long id) {
+//        Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(id);
+//        if (optionalMemberEntity.isPresent()) {
+//            return MemberDTO.toMemberDTO(optionalMemberEntity.get());
+//        }else {
+//            return null;
+//        }
+//    }
+//
+//    public MemberDTO updateForm(String myEmail) {
+//        Optional<MemberEntity> optionalMemberEntity = memberRepository.findByMemberEmail(myEmail);
+//        if(optionalMemberEntity.isPresent()) {
+//            return MemberDTO.toMemberDTO(optionalMemberEntity.get());
+//        } else {
+//            return null;
+//        }
+//    }
+//
+//    public void update(MemberDTO memberDTO) {
+//        memberRepository.save(MemberEntity.toMemberEntity(memberDTO));
+//    }
+//
+//    public void deleteById(Long id) {
+//        memberRepository.deleteById(id);
+//    }
 }
