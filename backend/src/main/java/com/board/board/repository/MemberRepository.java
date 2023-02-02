1a6beacbf1c1dc82046787a4468898436803eb24
zzzdks760 <zzzdks760@naver.com>
2023-02-02 16:04:58 +0900

    ,

--- backend/src/main/java/com/board/board/repository/MemberRepository.java
+++ backend/src/main/java/com/board/board/repository/MemberRepository.java
@@ -2,9 +2,13 @@ package com.board.board.repository;
 
 import com.board.board.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
     Optional<MemberEntity> findByMemberEmail(String memberEmail);
+    List<MemberEntity> findAll();
 }
