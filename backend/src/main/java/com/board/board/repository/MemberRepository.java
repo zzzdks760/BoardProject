1c0e3c7a57e577d105fa9c62bb72fa481f979c18
zzzdks760 <zzzdks760@naver.com>
2023-02-02 16:10:45 +0900

    .

--- backend/src/main/java/com/board/board/repository/MemberRepository.java
+++ backend/src/main/java/com/board/board/repository/MemberRepository.java
@@ -10,5 +10,4 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
     Optional<MemberEntity> findByMemberEmail(String memberEmail);
-    List<MemberEntity> findAll();
 }
