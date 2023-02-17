725cfed8bb95587d8806769fa0737db0415debf0
zzzdks760 <zzzdks760@naver.com>
2023-02-17 13:51:31 +0900

    .

--- backend/src/main/java/com/board/board/controller/CommentController.java
+++ backend/src/main/java/com/board/board/controller/CommentController.java
@@ -16,11 +16,16 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("board/comments")
-    public List<CommentDTO> save(@RequestBody CommentDTO commentDTO) {
+    public String save(@RequestBody CommentDTO commentDTO) {
         System.out.println("commentDTO = " + commentDTO);
         Long saveResult = commentService.save(commentDTO);
-
-        List<CommentDTO> commentDTOList = commentService.findAll(commentDTO.getBoardId());
-        return commentDTOList;
+        if (saveResult != null) {
+            // 작성 성공하면 댓글목록을 가져와서 리턴
+            // 댓글목록 : 해당 게시글의 댓글 전체(해당게시글 id를 기준으로 댓글전체를 가져옴)
+            List<CommentDTO> commentDTOList = commentService.findAll(commentDTO.getBoardId());
+            return "ok";
+        } else {
+            return null;
+        }
     }
 }
